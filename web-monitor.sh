2d2944310bde0fcad0579034a629067774753d2a
Justus0405 <justus040506@gmail.com>
2025-09-09 15:42:04 +0200

    Update web-monitor.sh

--- web-monitor.sh
+++ web-monitor.sh
@@ -6,28 +6,52 @@
 # Date: 13.02.2025
 # License: MIT
 
-# Global variables
-export version="1.0"
+export scriptVersion="1.0"
+
+## USER CONFIGURATION START
+
 export port="8080"
 
-# Color variables
+## USER CONFIGURATION STOP
+
+### COLOR CODES ###
+export black="\e[1;30m"
 export red="\e[1;31m"
 export green="\e[1;32m"
 export yellow="\e[1;33m"
+export blue="\e[1;34m"
+export purple="\e[1;35m"
 export cyan="\e[1;36m"
-export gray="\e[1;90m"
+export white="\e[1;37m"
 export bold="\e[1m"
-export endColor="\e[0m"
+export reset="\e[0m"
 
-# Info variables
-export success="${gray}[${green}✓${gray}]${endColor}"
-export error="${red}Error:${endColor}"
-export warning="${gray}[${red}!${gray}]${endColor}"
-export section="${gray}[${yellow}!${gray}]${endColor}"
-export info="${gray}[${cyan}i${gray}]${endColor}"
+### FUNCTIONS ###
+logMessage() {
+    local type=$1
+    local message=$2
+    case "${type}" in
+    "info" | "INFO")
+        echo -e "[  ${cyan}INFO${reset}  ] ${message}"
+        ;;
+    "done" | "DONE")
+        echo -e "[  ${green}DONE${reset}  ] ${message}"
+        exit 0
+        ;;
+    "warning" | "WARNING")
+        echo -e "[ ${red}FAILED${reset} ] ${message}"
+        ;;
+    "error" | "ERROR")
+        echo -e "[  ${red}ERROR${reset} ] ${message}"
+        exit 1
+        ;;
+    *)
+        echo -e "[UNDEFINED] ${message}"
+        ;;
+    esac
+}
 
-# Functions
-checkArgs() {
+checkArguments() {
     case $1 in
     "start")
         startService
@@ -40,21 +64,19 @@ checkArgs() {
         startService
         ;;
     "status")
-        showStatus
+        printStatus
         ;;
     "help")
-        showHelp
+        printHelp
         ;;
     "version")
-        showVersion
+        printVersion
         ;;
     "")
-        echo -e "${error} no operation specified. Use $(basename "$0") help"
-        exit 1
+        logMessage "error" "No operation specified. Use $(basename "$0") help"
         ;;
     *)
-        echo -e "${error} unrecognized option '$1'. Use $(basename "$0") help"
-        exit 1
+        logMessage "error" "Unrecognized option '$1'. Use $(basename "$0") help"
         ;;
     esac
 }
@@ -69,11 +91,11 @@ findTempPath() {
         "/sys/class/hwmon/hwmon1/temp2_input"
     )
     for path in "${potentialPaths[@]}"; do
-        if [[ -f "$path" ]]; then
+        if [[ -f "${path}" ]]; then
             # Check if the sensor returns a valid value
-            tempValue=$(cat "$path")
-            if [[ $tempValue =~ ^[0-9]+$ ]]; then
-                tempPath="$path"
+            tempValue=$(cat "${path}")
+            if [[ ${tempValue} =~ ^[0-9]+$ ]]; then
+                tempPath="${path}"
                 return 0
             fi
         fi
@@ -81,34 +103,34 @@ findTempPath() {
 }
 
 getData() {
-    # System Overview
-    showHostname=$(cat /etc/hostname)
-    showKernel=$(uname -r)
-    showUptime=$(uptime -p | sed 's/up //')
+    ### System Overview
+    export showHostname=$(cat /etc/hostname)
+    export showKernel=$(uname -r)
+    export showUptime=$(uptime -p | sed 's/up //')
     if [[ -f "/etc/machine-id" ]]; then
-        showAge=$((($(date +%s) - $(date -r "/etc/machine-id" +%s)) / 86400))
+        export showAge=$((($(date +%s) - $(date -r "/etc/machine-id" +%s)) / 86400))
     else
-        showAge="Error: File not found"
+        export showAge="Error: File not found"
     fi
-    showCpu=$(uptime | awk -F 'load average:' '{ print $2 }' | xargs)
-    showCpuTemp=$(($(cat "$tempPath") / 1000))
-    showRam=$(free -m | awk 'NR==2{used=$3; total=$2; printf "%dmb / %dmb (%.0f%%)", used, total, used/total*100}')
-    showDiskFull=$(df -h --total | awk '/total/ {printf "%s / %s (%s)", $3, $2, $5}')
-    showLocalIp=$(ip -4 addr show | grep -oP '(?<=inet\s)\d+(\.\d+){3}' | grep -v '127.0.0.1' | head -n 1)
+    export showCpu=$(uptime | awk -F 'load average:' '{ print $2 }' | xargs)
+    export showCpuTemp=$(($(cat "$tempPath") / 1000))
+    export showRam=$(free -m | awk 'NR==2{used=$3; total=$2; printf "%dmb / %dmb (%.0f%%)", used, total, used/total*100}')
+    export showDiskFull=$(df -h --total | awk '/total/ {printf "%s / %s (%s)", $3, $2, $5}')
+    export showLocalIp=$(ip -4 addr show | grep -oP '(?<=inet\s)\d+(\.\d+){3}' | grep -v '127.0.0.1' | head -n 1)
 
-    # System Stats
-    showCpuStat=$(lscpu | grep "Model name" | sed 's/Model name: //')
-    cpuPercentage=$(top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print 100 - $1}')
+    ### System Stats
+    export showCpuStat=$(lscpu | grep "Model name" | sed 's/Model name: //')
+    export cpuPercentage=$(top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print 100 - $1}')
 
-    showRamStat=$(free -g | awk 'NR==2{used=$3; total=$2; printf "%dGb / %dGb (%.0f%%)", used, total, used/total*100}')
-    ramPercentage=$(free -m | awk 'NR==2{used=$3; total=$2; printf "%.0f", used/total*100}')
+    export showRamStat=$(free -g | awk 'NR==2{used=$3; total=$2; printf "%dGb / %dGb (%.0f%%)", used, total, used/total*100}')
+    export ramPercentage=$(free -m | awk 'NR==2{used=$3; total=$2; printf "%.0f", used/total*100}')
 
-    showDiskRoot=$(df -h / | awk 'NR==2{printf "%s / %s (%s)", $3, $2, $5}')
+    export showDiskRoot=$(df -h / | awk 'NR==2{printf "%s / %s (%s)", $3, $2, $5}')
 
-    showNetworkStat=$(lspci | grep -i "Ethernet" | sed 's/.*Ethernet controller: //' | cut -c 1-32)
+    export showNetworkStat=$(lspci | grep -i "Ethernet" | sed 's/.*Ethernet controller: //' | cut -c 1-32)
 
-    # Footer
-    currentTime=$(date +"%d-%m-%Y %H:%M:%S")
+    ### Footer
+    export currentTime=$(date +"%d-%m-%Y %H:%M:%S")
 }
 
 exportResponse() {
@@ -298,48 +320,46 @@ exportResponse() {
 
 startService() {
     if [ -f ".serverPid" ]; then
-        echo -e "${error} Server is already running."
-        exit 1
+        logMessage "error" "Server is already running."
     fi
     findTempPath
     while :; do
         getData
         exportResponse
-        echo -e "HTTP/1.1 200 OK\nContent-Type: text/html\n\n$response" | nc -l -k -p $port -q 1 || {
-            echo -e "${error} netcat is not installed. Please install it either 'netcat-openbsd' or 'openbsd-netcat'"
-            exit 1
+        echo -e "HTTP/1.1 200 OK\nContent-Type: text/html\n\n${response}" | nc -l -k -p ${port} -q 1 || {
+            logMessage "error" "netcat is not installed. Please install it either 'netcat-openbsd' or 'openbsd-netcat'"
         }
     done &
     echo $! >".serverPid"
-    echo -e "${success} Started server on port: $port"
+    logMessage "done" "Started server on port: ${port}"
 }
 
 stopService() {
     if [ -f ".serverPid" ]; then
         pid=$(cat ".serverPid")
-        if kill -9 "$pid" >/dev/null 2>&1; then
-            echo -e "${success} Stopped server with PID: $pid"
+        if kill -9 "${pid}" >/dev/null 2>&1; then
             rm -f ".serverPid"
+            logMessage "info" "Stopped server with PID: ${pid}"
         else
-            echo -e "${error} No PID associated with that process. Did the server crash? Removing Cache..."
             rm -f ".serverPid"
+            logMessage "error" "No PID associated with that process. Did the server crash? Removing Cache..."
         fi
     else
-        echo -e "${info} No server PID found. Server is not running."
+        logMessage "info" "No server PID found. Server is not running."
     fi
 }
 
-showStatus() {
+printStatus() {
     if [ -f ".serverPid" ]; then
         pid=$(cat ".serverPid")
-        echo -e "${info} Server is running with PID: $pid"
+        logMessage "info" "Server is running with PID: ${pid}"
     else
-        echo -e "${info} No server PID found. Server is not running."
+        logMessage "info" "No server PID found. Server is not running."
     fi
     exit 0
 }
 
-showHelp() {
+printHelp() {
     echo -e "usage: $(basename "$0") [...]"
     echo -e "arguments:"
     echo -e "    start"
@@ -352,13 +372,12 @@ showHelp() {
     exit 0
 }
 
-showVersion() {
-    echo -e "               $(basename "$0") v${version} - GNU bash, version 5.3"
+printVersion() {
+    echo -e "               $(basename "$0") v${scriptVersion} - GNU bash, version 5.3"
     echo -e "               Copyright (C) 2025-present Justus0405"
     echo -e ""
     exit 0
 }
 
-# PROGRAM START
-
-checkArgs "$@"
+### PROGRAM START ###
+checkArguments "$@"
